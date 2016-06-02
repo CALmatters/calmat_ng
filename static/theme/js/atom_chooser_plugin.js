20a2e772bca847c9849200dd9383431e842d5efc
Gene Campbell <campbell.gene@gmail.com>
2016-06-02 16:43:55 -0700

    Atom chooser on menu bar too.

--- static/theme/js/atom_chooser_plugin.js
+++ static/theme/js/atom_chooser_plugin.js
@@ -1,3 +1,16 @@
+
+function handle_click(editor) {
+
+  editor.windowManager.open(
+      {
+        file: '/static/theme/js/atom_chooser.html',
+        width: 520 + parseInt(editor.getLang('example.delta_width', 0)),
+        height: 350 + parseInt(editor.getLang('example.delta_height', 0)),
+        inline: 1
+      });
+}
+
+
 tinymce.PluginManager.add('atoms', function(editor, url) {
 
 
@@ -5,19 +18,8 @@ tinymce.PluginManager.add('atoms', function(editor, url) {
     text: 'Insert Atoms',
     icon: false,
     onclick: function() {
-      // Open window
-      editor.windowManager.open({
-        title: 'Insert Atoms Button',
-        body: [
-          {type: 'textbox', name: 'title', label: 'Title'}
-        ],
-        onsubmit: function(e) {
-          // Insert content when the window form is submitted
-          editor.insertContent('Title: ' + e.data.title);
-        }
-      });
-    }
-  });
+      handle_click(editor);
+    }});
 
   // Adds a menu item to the tools menu
   editor.addMenuItem('atoms',
@@ -25,17 +27,7 @@ tinymce.PluginManager.add('atoms', function(editor, url) {
     text: 'Insert Atoms',
     context: 'tools',
     onclick: function() {
-      // Open window with a specific url
-      editor.windowManager.open(
-          {
-            file: '/static/theme/js/atom_chooser.html',
-            width: 520 + parseInt(editor.getLang('example.delta_width', 0)),
-            height: 350 + parseInt(editor.getLang('example.delta_height', 0)),
-            inline: 1
-          }, {
-            plugin_url: url, // Plugin absolute URL
-            some_custom_arg: 'custom arg' // Custom argument
-          });
+        handle_click(editor);
     }}
   );
 
