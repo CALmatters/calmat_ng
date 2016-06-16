235d6b14b283e4b2df09282409ae1d4e05880033
Gene Campbell <campbell.gene@gmail.com>
2016-06-16 13:16:19 -0700

    home page update for article image updates, fixed up image converter to deal with missing images

--- calmat_ng/management/commands/migrate_images.py
+++ calmat_ng/management/commands/migrate_images.py
@@ -17,8 +17,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        print(options)
-
         src_field = options['src']
         dst_field = options['dst']
         app_name, model_name = options['model'].split(':')
@@ -26,33 +24,42 @@ class Command(BaseCommand):
         Model = apps.get_model(app_name, model_name)
 
         for obj in Model.objects.all():
-            from_path = os.path.join(
-                settings.MEDIA_ROOT, getattr(obj, src_field).path)
-            fname = from_path.split('/')[-1]
-            to_path_rel = os.path.join('uploads/', fname)
-            to_path = os.path.join(settings.MEDIA_ROOT, to_path_rel)
+            print("Processing {}".format(obj.title))
 
-            print(from_path, to_path_rel, to_path)
             try:
-                shutil.move(from_path, to_path)
-            except FileNotFoundError:
-                pass
+                src_path_rel = getattr(obj, src_field).path
+            except ValueError:
+                print("No image for '{}' on {} {}".format(
+                    src_field, model_name, obj.title))
+            else:
+                src_path_abs = os.path.join(settings.MEDIA_ROOT, src_path_rel)
 
-            default_caption = '{} {} image'.format(obj.title, model_name)
-            defaults = dict(caption=default_caption, alt_tag=default_caption)
+                fname = src_path_abs.split('/')[-1]
+                to_path_rel = os.path.join('uploads/', fname)
+                to_path_abs = os.path.join(settings.MEDIA_ROOT, to_path_rel)
 
-            item, created = MediaItem.objects.get_or_create(
-                file=to_path_rel, defaults=defaults)
-            print(created, item)
+                print(src_path_abs, to_path_rel, to_path_abs)
+                try:
+                    shutil.move(src_path_abs, to_path_abs)
+                except FileNotFoundError:
+                    pass
 
-            if not created:
-                item.caption = default_caption
-                item.alt_tag = default_caption
-                item.save()
+                default_caption = '{} {} image'.format(obj.title, model_name)
+                defaults = dict(
+                    caption=default_caption, alt_tag=default_caption)
 
-            if getattr(obj, dst_field) != item:
-                setattr(obj, dst_field, item)
-                obj.save()
+                item, created = MediaItem.objects.get_or_create(
+                    file=to_path_rel, defaults=defaults)
+                print(created, item)
+
+                if not created:
+                    item.caption = default_caption
+                    item.alt_tag = default_caption
+                    item.save()
+
+                if getattr(obj, dst_field) != item:
+                    setattr(obj, dst_field, item)
+                    obj.save()
 
 
 
