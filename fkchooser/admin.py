d4777c18e25d528d3dc1cea5dffd8973b15beb85
Gene Campbell <campbell.gene@gmail.com>
2016-06-28 16:55:12 -0400

    Only write popup_fk_controller.js to popped up windows, and only write onece.

--- fkchooser/admin.py
+++ fkchooser/admin.py
@@ -4,16 +4,6 @@ from django.utils.safestring import mark_safe
 
 class FKChooserAdminMixin(admin.ModelAdmin):
 
-    class Media:
-        js = (
-            #  Todo:  Conditional - only for popup
-            'fk_static_files/popup_fk_controller.js',
-        )
-        css = {
-            'all': (
-            )
-        }
-
     def _fb_file_link(self, obj):
         fb_link = mark_safe(
             '<button data-fk-id="{}">Select</button>'.format(obj.id))
@@ -35,3 +25,16 @@ class FKChooserAdminMixin(admin.ModelAdmin):
             return super(
                 FKChooserAdminMixin, self).get_list_display_links(
                 request, list_display)
+
+    def changelist_view(self, request, extra_context=None):
+
+        template_response = super(FKChooserAdminMixin, self).changelist_view(
+            request, extra_context)
+
+        #  if this request leads to a popup (really should be our popup)
+        #  add the popup controller codd to the response media
+        if request.GET.get('_popup', False):
+            template_response.context_data['media']._js.append(
+                'fk_static_files/popup_fk_controller.js')
+
+        return template_response
