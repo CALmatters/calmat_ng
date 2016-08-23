7d60415a18dd379a1dd3f118bf7cd1dd9b09bc23
Gene Campbell <campbell.gene@gmail.com>
2016-08-23 09:11:52 -0700

    New action in Distribution admin to download all 2016 distribution in CSV

--- cmskit/mixins/exportable.py
+++ cmskit/mixins/exportable.py
@@ -1,7 +1,23 @@
 import csv
+import datetime
 
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
+from django.db import connection
+
+DISTRIBUTIONS_2016_SQL = """
+SELECT distinct a.title, c.title AS publisher, f.title AS owner,
+a.publish_date, b.date_published, u.username
+FROM business_partnerarticle b
+FULL OUTER JOIN pages_article a ON b.article_id=a.id
+FULL OUTER JOIN business_partner c ON b.partner_id=c.id
+FULL OUTER JOIN pages_article_authors d ON a.id=d.article_id
+FULL OUTER JOIN business_person e ON d.person_id = e.id
+FULL OUTER JOIN auth_user u ON e.user_id = u.id
+FULL OUTER JOIN business_partnerowner f ON c.owner_id = f.id
+WHERE a.publish_date > '2016-01-01'
+AND b.fulltext_or_mention = 'Full Text Publish'
+ORDER by a.publish_date;"""
 
 
 class AdminCSVExport(object):
@@ -26,4 +42,32 @@ class AdminCSVExport(object):
 
     export_as_csv.short_description = "Export selected objects as csv file"
 
-    actions = [export_as_csv, ]
+    @staticmethod
+    def yield_distribution_rows(year='2016'):
+        cursor = connection.cursor()
+
+        cursor.execute(DISTRIBUTIONS_2016_SQL)
+
+        for row in cursor.fetchall():
+            yield row
+
+    def export_2016_distrubutions(modeladmin, request, queryset):
+
+        if not request.user.is_staff:
+            raise PermissionDenied
+
+        opts = modeladmin.model._meta
+        response = HttpResponse(content_type='text/csv')
+        file_name = "%s_%s" % (
+            opts.verbose_name.lower(), datetime.datetime.now())
+        response[
+            'Content-Disposition'] = 'attachment; filename=%s.csv' % file_name
+        writer = csv.writer(response)
+
+        for row in AdminCSVExport.yield_distribution_rows():
+            writer.writerow(row)
+        return response
+
+    export_2016_distrubutions.short_description = "Export 2016 distributions"
+
+    actions = [export_as_csv, export_2016_distrubutions]
